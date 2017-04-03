174de394af23e575d9d2562e766a024315e0c2bd
Che Sun <jazzboysc@gmail.com>
2017-04-04 00:33:39 +0800

    ...

--- SwingEngine/SERayTracingAdapter/Device/SERayTracingDevice.h
+++ SwingEngine/SERayTracingAdapter/Device/SERayTracingDevice.h
@@ -47,6 +47,55 @@ typedef void (SERayTracingDevice::*RayTracingDeviceInitialize)(SERayTracingDevic
 typedef void (SERayTracingDevice::*RayTracingDeviceTerminate)();
 
 //----------------------------------------------------------------------------
+class SERayTracingDeviceDelegate1
+{
+    typedef void (*tstub)(void* pobject, SERayTracingDevice& rtDevice, void* userObj);
+    typedef void (*tfunc)(SERayTracingDevice& rtDevice, void* userObj);
+
+public:
+    SERayTracingDeviceDelegate1() {}
+
+    template <class T, void (T::*TMethod)(SERayTracingDevice&, void*)>
+    static SERayTracingDeviceDelegate1 FromMethod(T* pobject, void *puserObj)
+    {
+        return SERayTracingDeviceDelegate1(pobject, &MethodStub<T, TMethod>, puserObj);
+    }
+
+    static SERayTracingDeviceDelegate1 FromFunction(tfunc pfunc, void *puserObj)
+    {
+        return SERayTracingDeviceDelegate1(pfunc, puserObj);
+    }
+
+    void operator()(SERayTracingDevice& rtDevice) const
+    {
+        return pobject ? (*pmethod.pstub)(pobject, rtDevice, puserObj) : (*pmethod.pfunc)(rtDevice, puserObj);
+    }
+
+private:
+    void* pobject;
+    union tmethod
+    {
+        tstub pstub;
+        tfunc pfunc;
+        tmethod() : pstub() {}
+        tmethod(tstub pstub) : pstub(pstub) {}
+        tmethod(tfunc pfunc) : pfunc(pfunc) {}
+    } pmethod;
+    void* puserObj;
+
+    template <class T, void (T::*TMethod)(SERayTracingDevice&, void*)>
+    static void MethodStub(void* pobject, SERayTracingDevice& rtDevice, void *puserObj)
+    {
+        T* p = static_cast<T*>(pobject);
+        return (p->*TMethod)(rtDevice, puserObj);
+    }
+
+    SERayTracingDeviceDelegate1(tfunc pfunc, void *puserObj) : pobject(nullptr), pmethod(pfunc), puserObj(puserObj) {}
+    SERayTracingDeviceDelegate1(void *pobject, tstub pstub, void *puserObj) : pobject(pobject), pmethod(pstub), puserObj(puserObj) {}
+};
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
 // Author: Che Sun
 // Date: 03/20/2017
 //----------------------------------------------------------------------------
@@ -64,6 +113,10 @@ public:
 protected:
     RayTracingDeviceInitialize               _Initialize;
     RayTracingDeviceTerminate                _Terminate;
+
+    SERayTracingDeviceDelegate1  mRenderStartDelegate;
+    SERayTracingDeviceDelegate1  mImageReadyDelegate;
+    SERayTracingDeviceDelegate1  mDeviceCloseDelegate;
 
     SERayTracingDeviceDescription mDeviceDesc;
     SERayTracingDeviceVendor mDeviceVendor;
